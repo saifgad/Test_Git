6b834c5f5fbc2c925563117ac7a6a896329b3e5e
saifgad <saifgad1107@gmail.com>
2022-08-18 15:30:57 +0200

    2.0.0

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 int main()
 {
     int a=4,b=5;
-    int sum=Sum(a,b);
-    printf("%d\n",sum);
+    int dum=Sum(a,b);
+    printf("%d\n",dum);
     int sub=Sub(a,b);
     printf("%d\n",sub);
     int mul=Mul(a,b);
     printf("%d",mul);
+    int div=Div(a,b);
+    printf("%d",div);
     return 0;
 }
